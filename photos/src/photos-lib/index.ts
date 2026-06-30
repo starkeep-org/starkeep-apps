08db68d988e141b1f5184fd72282558951fdf343
Aaron Koller <amkoller@Aarons-MacBook-Pro.local>
2026-06-30 15:49:18 -0400

    tweaks

--- photos/src/photos-lib/index.ts
+++ photos/src/photos-lib/index.ts
@@ -2,13 +2,6 @@
 export type { AppImage, AppImageExif } from "./types/app-image";
 export type { GoogleAlbum, GoogleMediaItem } from "./types/google";
 
-// Data helpers
-export { IMAGE_RECORD_TYPE } from "./data/image-record";
-
 // EXIF reader
 export { extractExif } from "./metadata/exif-reader";
 export type { ExifFields } from "./metadata/exif-generator";
-
-// App manifest constants and bootstrap
-export { PHOTOS_APP_ID, PHOTOS_APP_RECORD_TYPES } from "./manifest";
-export { bootstrapPhotosApp } from "./bootstrap";
